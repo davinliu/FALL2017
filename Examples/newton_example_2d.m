f72ebf71ad27a5d7c9a030b5ab3102b15d5143bb
Ivan Rudik <irudik@iastate.edu>
2016-02-05 08:46:00 -0600

    Update Newton example with plot.

--- Examples/newton_example_2d.m
+++ Examples/newton_example_2d.m
@@ -8,16 +8,22 @@ e = -1/eta;
 
 tol = 1e-5;
 maxit = 1000;
-q = [1; 1];
+q = [10; 35];
 
 for i = 1:maxit
+    q_store(:,i) = q;
+    % FOC system
     fval = sum(q)^e + e*sum(q)^(e-1)*q - diag(c)*q;
+    % Jacobian
     fjac = e*sum(q)^(e-1)*ones(2,2) + e*sum(q)^(e-1)*eye(2) + ...
         (e-1)*e*sum(q)^(e-2)*q*[1 1] - diag(c);
+    % Newton step
     q = q - fjac\fval;
+    % Break if within tolerance
     if norm(fval) < tol
         break
     end
 end
-
+q_store(:,i+1) = q;
 disp(['Converged to solution q = ' mat2str(q,4) ' in ' num2str(i) ' iterations.']);
+scatter(q_store(1,:),q_store(2,:),'filled');
