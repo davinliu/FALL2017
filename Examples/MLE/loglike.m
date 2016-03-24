fba1c783a2ed258e17dc71b065ca5ad2e5973e47
Ivan Rudik <irudik@iastate.edu>
2016-03-24 08:27:45 -0500

    Added 2 dimensional extraction example, problem set 5 for MLE/IV.

--- Examples/MLE/loglike.m
+++ Examples/MLE/loglike.m
@@ -1,10 +1,10 @@
 function nloglikelihood = loglike(Y,X,rho)
     % Coefficients
-    beta = rho(1:4);
+    beta = rho(1:end-1);
     
     % Ensure positive variance guesses, will have to re-transform the
     % maximizing guess in the main_script
-    sigma2 = exp(rho(5));
+    sigma2 = exp(rho(end));
     
     % Residual in equation
     residual = Y - X*beta;
